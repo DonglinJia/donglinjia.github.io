5db06c99124bc02bf18df9eba29fdffa0f097542
Leo <leo@v1040-wn-rt-c-51-235.campus-dynamic.uwaterloo.ca>
2019-05-16 14:57:58 -0400

    New change

--- build.sh
+++ build.sh
@@ -7,17 +7,17 @@ if [ $? -ne 0 ]; then
 	echo "Error at add"
 	exit 1
 fi
-
+echo "All adds are successful!"
 git commit -m "New change"
 if [ $? -ne 0 ]; then
 	echo "Error at commit"
 	exit 1
 fi
-
+echo "Commit successfully!"
 git push origin master
 if [ $? -ne 0 ]; then
 	echo "Error at push"
 	exit 1
 fi
-
+echo "Push successfully!"
 echo "Update Successfully!"
